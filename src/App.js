0ac6c04206fe27d98068905cd42984631de977de
Caroline1803 <55406859+Caroline1803@users.noreply.github.com>
2021-04-22 17:51:53 -0300

    aula 3 e 4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+
+import React, { useState } from "react";
+
 import "./styles.css";
 import Navbar from "./components/Navbar/Navbar";
-import TaskList from "./components/TaskList/TaskList";
+import TaksList from "./components/TaskList/TaskList";
+// definindo objetos
+const task = {
+  id: 0,
+  title: "Nova Tarefa",
+  state: "pendente"
+};
+let idAcc = 0;
+const generateId = () => {
+  idAcc = idAcc = 1;
+  return idAcc;
+};
+
 
 export default function App() {
   return (
@@ -12,6 +26,33 @@ export default function App() {
         <TaskList title="Fazendo" />
         <TaskList title="Completo" />
       </div>
+    </div>
+  );
+}
+
+  const [tasks, setTasks] = useState([]); //array
+
+  const addTask = (title, state) => {
+    const newTask = {
+      id: generateId(),
+      title,
+      state
+    };
+    setTasks((existingTask) => {
+      return [...existingTask, newTask];
+    });
+  };
+
+  return (
+    <div className="App">
+      <Navbar />
+      <ul className="lista">
+        <div className="container">
+          <TaksList title="Pendente" onAddTask={addTask} tasks={tasks} />
+          <TaksList title="Fazendo" onAddTask={addTask} tasks={tasks} />
+          <TaksList title="Completo" onAddTask={addTask} tasks={tasks} />
+        </div>
+      </ul>
     </div>
   );
 }
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,9 +1,21 @@
-import React from "react";
+import "./tasklist.css";
+import PropTypes from "prop-types";
 
-export default function TaskList(props) {
+export default function TaskList({ title, onAddTask, tasks }) {
+  const AddTask = () => {
+    onAddTask("Nova Tarefa", "Pendente");
+  };
+
   return (
-    <div>
-      <div>Titulo</div>
+    <div className="tasklist">
+      <div className="title">{title}</div>
+      <div className="content"></div>
+      <button onClick={AddTask}>Adicionar Tarefa</button>
     </div>
   );
 }
+
+TaskList.propTypes = {
+  title: PropTypes.string.isRequired,
+  onAddTask: PropTypes.func.isRequired
+};
